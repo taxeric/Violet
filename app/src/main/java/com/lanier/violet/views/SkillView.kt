2bdd275b93766bd02bb2ea2c87389261863900dd
幻弦让叶 <51876689+taxeric@users.noreply.github.com>
2024-07-08 23:24:56 +0800

    optimization: skill view

--- app/src/main/java/com/lanier/violet/views/SkillView.kt
+++ app/src/main/java/com/lanier/violet/views/SkillView.kt
@@ -1,11 +1,15 @@
 package com.lanier.violet.views
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.util.AttributeSet
 import android.view.View
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.lanier.violet.R
+import com.lanier.violet.database.data.SkillHelper
+import com.lanier.violet.database.entity.Skill
 import com.lanier.violet.feature.pet.data.SkillData
 
 /**
@@ -19,21 +23,53 @@ class SkillView @JvmOverloads constructor(
 ) : ConstraintLayout(context, attributeSet, defStyleAttr) {
 
     private var tvInfo: TextView
+    private var skill: Skill? = null
 
     init {
         View.inflate(context, R.layout.view_skill, this)
         tvInfo = findViewById(R.id.tvSkillInfo)
+        tvInfo.setOnClickListener { detailsDialog() }
     }
 
     fun bind(skillData: SkillData?) {
-        tvInfo.text = if (skillData == null) {
-            context.getString(R.string.__empty)
+        if (skillData == null) {
+            tvInfo.text = context.getString(R.string.__empty)
         } else {
-            buildString {
-                append(context.getString(R.string.m_id, skillData.id))
-                append("\n")
-                append(context.getString(R.string.m_skill_pp, "${skillData.pp}"))
-            }
+            SkillHelper.getSkillByHexId(
+                skillData.id,
+                onSuccess = { dbSkill ->
+                    this.skill = dbSkill
+                    tvInfo.text = buildString {
+                        append(dbSkill.name.ifEmpty { context.getString(R.string.__unknown) })
+                        append("\n")
+                        append(skillData.pp)
+                        append("/")
+                        append(dbSkill.ppMax)
+                        append("\n")
+                        append(context.getString(R.string.attack_speed, dbSkill.speed))
+                        append("\n")
+                        append(context.getString(R.string.skill_power, dbSkill.power))
+                    }
+                },
+                onFailure = {
+                    tvInfo.text = buildString {
+                        append(context.getString(R.string.m_id, skillData.id))
+                        append("\n")
+                        append(context.getString(R.string.m_skill_pp, "${skillData.pp}"))
+                    }
+                }
+            )
+        }
+    }
+
+    private fun detailsDialog() {
+        skill?.let {
+            val onClickListener = DialogInterface.OnClickListener { dialog, which -> dialog?.dismiss() }
+            AlertDialog.Builder(context)
+                .setTitle(it.name)
+                .setMessage(it.description)
+                .setPositiveButton(context.getString(R.string.dialog_sure), onClickListener)
+                .show()
         }
     }
 }
